0ae6d64663fbde4cf3c2e95c2791d0b4be8e3241
Tabata Santos <tabata.gabriela@hotmail.com>
2025-01-23 20:41:46 -0300

    adicionando widget card

---
+++ lib/views/presentation/widgets/characters_card_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CharacterCard extends StatelessWidget {
+  final String name;
+  final String thumbnail;
+
+  const CharacterCard({
+    super.key,
+    required this.name,
+    required this.thumbnail,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 156,
+      height: 156,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        elevation: 4,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.white.withOpacity(0),
+                Colors.grey.withOpacity(0.8),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Image.network(
+                  thumbnail,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Text(
+                name,
+                style: GoogleFonts.roboto(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
