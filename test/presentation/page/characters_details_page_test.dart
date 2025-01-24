35e245073dca5c1e01bc3a2c8c4c5955f50895f4
Tabata Santos <tabata.gabriela@hotmail.com>
2025-01-24 07:00:40 -0300

    finalizando teste unitário

--- test/presentation/page/characters_details_page_test.dart
+++ test/presentation/page/characters_details_page_test.dart
@@ -5,7 +5,6 @@ import 'package:marvel_app/viewModel/models/characters_model.dart';
 import 'package:marvel_app/views/presentation/pages/characters_details_page.dart';
 import 'package:mocktail/mocktail.dart';
 
-// Mock para o modelo de personagem
 class MockCharacterModel extends Mock implements CharacterModel {}
 
 void main() {
@@ -14,7 +13,6 @@ void main() {
   setUp(() {
     mockCharacter = MockCharacterModel();
 
-    // Definindo valores mockados para o personagem
     when(() => mockCharacter.name).thenReturn('Iron Man');
     when(() => mockCharacter.description)
         .thenReturn('Genius, billionaire, playboy, philanthropist.');
@@ -24,36 +22,26 @@ void main() {
 
   testWidgets('Deve exibir os detalhes do personagem corretamente',
       (WidgetTester tester) async {
-    // Constrói a página com o personagem mockado
     await tester.pumpWidget(
       MaterialApp(
         home: CharacterDetailPage(character: mockCharacter),
       ),
     );
 
-    // Verifica se a imagem do personagem está sendo exibida
     final imageFinder = find.byType(Image);
     expect(imageFinder, findsOneWidget);
 
-    // Verifica se o nome do personagem aparece na tela
     expect(find.text('Iron Man'), findsOneWidget);
 
-    // Verifica se a biografia do personagem está correta
     expect(find.text('BIOGRAPHY'), findsOneWidget);
     expect(find.text('Genius, billionaire, playboy, philanthropist.'),
         findsOneWidget);
 
-    // Verifica se o logo da Marvel está sendo exibido
-    expect(
-        find.byType(Image),
-        findsNWidgets(
-            2)); // Uma para a imagem do personagem e outra para o logo da Marvel
+    expect(find.byType(Image), findsNWidgets(2));
 
-    // Simula o clique no botão de voltar
     await tester.tap(find.byIcon(Icons.back_hand_outlined));
     await tester.pumpAndSettle();
 
-    // Verifica se a navegação foi realizada corretamente (pop do Navigator)
     expect(find.byType(CharacterDetailPage), findsNothing);
   });
 
@@ -65,7 +53,6 @@ void main() {
       ),
     );
 
-    // Verifica se a AppBar contém o logo da Marvel
     expect(find.byType(AppBar), findsOneWidget);
     expect(find.byType(Image), findsWidgets);
   });
--- test/viewModel/blocs/characters_bloc_test.mocks.dart
+++ test/viewModel/blocs/characters_bloc_test.mocks.dart
@@ -20,7 +20,6 @@ void main() {
     late CharacterModel mockCharacter;
 
     setUp(() {
-      // Instancia os mocks
       mockBloc = MockCharacterBloc();
       mockEmitter = MockEmitter();
       mockDio = MockDio();
@@ -29,7 +28,6 @@ void main() {
     });
 
     test('deve emitir CharacterLoading ao carregar personagens', () async {
-      // Configuração do mock do Dio
       final mockResponse = Response(
         requestOptions: RequestOptions(path: ''),
         data: {
@@ -47,43 +45,34 @@ void main() {
 
       when(() => mockDio.get(any())).thenAnswer((_) async => mockResponse);
 
-      // Simula o método getAllCharacters
       when(() => mockBloc.getAllCharacters(any(), any())).thenAnswer((_) async {
-        // Emite os estados simulados
         final response = await mockDio.get(any());
         final data = response.data['data']['results'] as List;
         final allCharacters =
             data.map((e) => CharacterModel.fromJson(e)).toList();
 
-        // Emite os estados usando o mockEmitter com a assinatura correta
         mockEmitter(CharacterLoaded(allCharacters, allCharacters, []));
       });
 
-      // Verificar se o método foi chamado corretamente
       verify(() => mockBloc.getAllCharacters(any(), any())).called(1);
     });
 
     test('deve emitir CharacterError ao falhar ao carregar personagens',
         () async {
-      // Configura o mock do Dio para lançar um erro
       when(() => mockDio.get(any())).thenThrow(Exception('Erro ao carregar'));
 
-      // Simula o método getAllCharacters com erro
       when(() => mockBloc.getAllCharacters(any(), any())).thenAnswer((_) async {
         try {
           await mockDio.get(any());
         } catch (e) {
-          // Emite um erro usando o mockEmitter com a assinatura correta
           mockEmitter(CharacterError('Erro ao carregar lista de personagens'));
         }
       });
 
-      // Verificar se o erro foi emitido
       verify(() => mockBloc.getAllCharacters(any(), any())).called(1);
     });
 
     test('deve filtrar personagens ao buscar por nome', () async {
-      // Dados de personagens mockados
       final characters = [
         CharacterModel(
             id: 1, name: 'Iron Man', description: 'A hero', thumbnail: ''),
@@ -94,35 +83,26 @@ void main() {
             thumbnail: ''),
       ];
 
-      // Simula a busca
       when(() => mockBloc.getSearchCharacters(any(), any())).thenAnswer((_) {
         const query = 'Iron';
         final filtered = characters
             .where((character) => character.name.contains(query))
             .toList();
 
-        // Emite o estado de pesquisa com o mockEmitter com a assinatura correta
         mockEmitter(CharacterLoaded(characters, filtered, []));
       });
 
-      // Ação: Simular a busca
-
-      // Verificar se o filtro foi aplicado corretamente
       verify(() => mockBloc.getSearchCharacters(any(), any())).called(1);
     });
 
     test('deve atualizar a lista de recentemente vistos', () async {
-      // Simula o evento de "RecentlyViewCharacter"
-
       when(() => mockBloc.getRecentlyViewCharacter(any(), any()))
           .thenAnswer((_) async {
         final updatedRecentlyViewed = [mockCharacter];
 
-        // Emite o estado de recentemente visualizados com o mockEmitter com a assinatura correta
         mockEmitter(CharacterLoaded([], [], updatedRecentlyViewed));
       });
 
-      // Verificar se o método foi chamado corretamente
       verify(() => mockBloc.getRecentlyViewCharacter(any(), any())).called(1);
     });
   });
